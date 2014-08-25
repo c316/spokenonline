f43e879a5a1127a27f68fed34a0976ab31dc1884
Joshua Bechard <josh.bechard@gmail.com>
2014-08-25 19:56:45 +0000

    Removed some tests

--- tests/tests.js
+++ tests/tests.js
@@ -16,21 +16,3 @@ suite('Email_List', function() {
   });
 });
 
-  test('using both client and the server', function(done, server, client) {
-    server.eval(function() {
-      Email_List.find().observe({
-        added: addedNewEmail
-      });
-
-      function addedNewEmail(email_list) {
-        emit('email_list', email_list);
-      }
-    }).once('email_list', function(email_list) {
-      assert.equal(email_list.fname, 'George');
-      done();
-    });
-
-    client.eval(function() {
-      Email_List.insert({fname: 'George'});
-    });
-  });
