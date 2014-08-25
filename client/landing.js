a1b16ac35f407657a6e571f79f55f8f23ac04c37
Joshua Bechard <josh.bechard@gmail.com>
2014-08-25 22:31:16 +0000

    Fixed design layout

--- client/landing.js
+++ client/landing.js
@@ -1,15 +1,15 @@
 Template.landing.events({
     'mouseover [name=attendPage]': function (e, tmpl) {
-    	$('.arrow-right').show();
-    },
-    'mouseover [name=viewPage]': function (e, tmpl) {
     	$('.arrow-left').show();
     },
+    'mouseover [name=viewPage]': function (e, tmpl) {
+    	$('.arrow-right').show();
+    },
     'mouseout [name=attendPage]': function (e, tmpl) {
-    	$('.arrow-right').hide();
+    	$('.arrow-left').hide();
     },
     'mouseout [name=viewPage]': function (e, tmpl) {
-    	$('.arrow-left').hide();
+    	$('.arrow-right').hide();
     },
 	'click [name=viewPage]': function() {
 		console.log("viewPage Clicked");
