c04df8d2105a5320228338036cbd04768cebb508
Joshua Bechard <josh.bechard@gmail.com>
2014-10-09 22:42:51 +0000

    Changes to failed method

--- client/spokenonline.js
+++ client/spokenonline.js
@@ -229,7 +229,12 @@ Template.stream.helpers({
 		}
 	},
 	show_other: function () {
-		return this.image;
+		if(this.failed === false){
+			return '<img src="/spoken/images/' + this.image + '">';	
+		}else{
+			return "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/108520028' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>'";
+		}
+		
 	}
 });
 Template.live.helpers({
